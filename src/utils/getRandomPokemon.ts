ed79f092fce199c9bac56c849a606a7cf0fdb6b5
Alberto J. Perez Roman <alberto.perez8@upr.edu>
2022-01-05 04:17:42 -0400

    random pokemon id fetcher

---
+++ src/utils/getRandomPokemon.ts
@@ -0,0 +1,17 @@
+const MAX_DEX = 649;
+
+export const getRandomPokemon: (notThisOne?: number) => number = (
+	notThisOne?: number
+) => {
+	const pokedexNum = Math.floor(Math.random() * MAX_DEX) + 1;
+
+	if (pokedexNum !== notThisOne) return pokedexNum;
+	return getRandomPokemon(notThisOne);
+};
+
+export const getOptionsForVote = () => {
+	const firstId = getRandomPokemon();
+	const secondId = getRandomPokemon(firstId);
+
+    return [firstId, secondId];
+};
