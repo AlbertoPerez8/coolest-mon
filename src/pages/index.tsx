925c845b68a0f873590ff0a2497916d39ea0d256
Alberto J. Perez Roman <alberto.perez8@upr.edu>
2022-01-05 03:48:18 -0400

    restruct folders and added libraries

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,13 @@
-import type { NextPage } from "next";
-
-const Home: NextPage = () => {
+export default function Home() {
 	return (
 		<div className="h-screen w-screen flex flex-col justify-center items-center">
 			<div className="text-2xl text-center">Select the coolest pokémon</div>
-      <div className="p-2"/>
+			<div className="p-2" />
 			<div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-				<div className="w-16 h-16 bg-yellow-400" />
+				<div className="w-16 h-16 bg-yellow-100" />
 				<div className="p-6">VS</div>
 				<div className="w-16 h-16 bg-blue-400" />
 			</div>
 		</div>
 	);
-};
-
-export default Home;
+}
