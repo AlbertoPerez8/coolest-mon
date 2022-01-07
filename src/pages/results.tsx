74dc4a990d49564815add2c556eed83f3b4e63a6
Alberto J. Perez Roman <alberto.perez8@upr.edu>
2022-01-07 05:11:40 -0400

    last commit before bed

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -4,24 +4,6 @@ import React from "react";
 import { AsyncReturnType } from "@/utils/ts-bs";
 import Image from "next/image";
 
-type PokemonQueryResult = AsyncReturnType<typeof getPokemonInOrder>;
-
-const PokemonListing: React.FC<{ pokemon: PokemonQueryResult[number] }> = (
-	props
-) => {
-	return (
-		<div className="flex">
-			<Image
-				src={props.pokemon.spriteUrl}
-				alt=""
-				width={256}
-				height={256}
-				layout="fixed"
-			/>
-		</div>
-	);
-};
-
 const getPokemonInOrder = async () => {
 	return prisma.pokemon.findMany({
 		orderBy: { VotesFor: { _count: "desc" } },
@@ -39,10 +21,24 @@ const getPokemonInOrder = async () => {
 	});
 };
 
-export const getServerSideProps: GetServerSideProps = async (props) => {
-	const pokemonOrdered = await getPokemonInOrder();
-	return { props: { pokemon: pokemonOrdered }, revalidate: 60 };
+type PokemonQueryResult = AsyncReturnType<typeof getPokemonInOrder>;
+
+const PokemonListing: React.FC<{ pokemon: PokemonQueryResult[number] }> = (
+	{pokemon}
+) => {
+	return (
+		<div className="flex">
+			<Image
+				src={pokemon.spriteUrl}
+				alt=""
+				width={256}
+				height={256}
+				layout="fixed"
+			/>
+		</div>
+	);
 };
+
 
 const ResultsPage: React.FC<{
 	pokemon: PokemonQueryResult;
@@ -58,3 +54,9 @@ const ResultsPage: React.FC<{
 };
 
 export default ResultsPage;
+
+
+export const getStaticProps: GetServerSideProps = async () => {
+	const pokemonOrdered = await getPokemonInOrder();
+	return { props: { pokemon: pokemonOrdered }, revalidate: 60 };
+};
