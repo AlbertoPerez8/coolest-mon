ea6ec3ce2647fff2a34d197cf89083bc8ac9b927
Alberto J. Perez Roman <alberto.perez8@upr.edu>
2022-01-11 02:17:06 -0400

    Results page

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -24,37 +24,39 @@ const getPokemonInOrder = async () => {
 type PokemonQueryResult = AsyncReturnType<typeof getPokemonInOrder>;
 
 const PokemonListing: React.FC<{ pokemon: PokemonQueryResult[number] }> = (
-	{pokemon}
+	props
 ) => {
 	return (
-		<div className="flex">
+		<div className="flex border-b items-center">
 			<Image
-				src={pokemon.spriteUrl}
+				src={props.pokemon.spriteUrl}
 				alt=""
-				width={256}
-				height={256}
+				width={128}
+				height={128}
 				layout="fixed"
 			/>
+			<div className="p-2" />
+			<div className="capitalize text-lg">{props.pokemon.name}</div>
 		</div>
 	);
 };
-
 
 const ResultsPage: React.FC<{
 	pokemon: PokemonQueryResult;
 }> = (props) => {
 	return (
-		<div className="flex flex-col">
-			<h2>Results</h2>
-			{props.pokemon.map((currentPokemon, index) => {
-				return <PokemonListing pokemon={currentPokemon} key={index} />;
-			})}
+		<div className="flex flex-col items-center">
+			<h2 className="text-2xl p-4">Results</h2>
+			<div className="flex flex-col w-full max-w-2xl border">
+				{props.pokemon.map((currentPokemon, index) => {
+					return <PokemonListing pokemon={currentPokemon} key={index} />;
+				})}
+			</div>
 		</div>
 	);
 };
 
 export default ResultsPage;
-
 
 export const getStaticProps: GetServerSideProps = async () => {
 	const pokemonOrdered = await getPokemonInOrder();
